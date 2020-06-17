f45d628fd954bd48a2e5b04c478c140de80839a8
Emmanuel Egomson <femmor2u@gmail.com>
2020-06-17 16:39:42 -0500

    added google-fonts, css-variables in App.css

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css"
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <>
+        <h1>Techazon</h1>
+    </>
   );
 }
 
