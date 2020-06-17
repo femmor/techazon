564d60b99effd81399e8017c1884b751c897b229
Emmanuel Egomson <femmor2u@gmail.com>
2020-06-17 16:59:23 -0500

    implemented styled-components

--- src/App.js
+++ src/App.js
@@ -5,12 +5,22 @@ import "bootstrap/dist/css/bootstrap.min.css"
 // FontAwesome
 import { FaHome } from "react-icons/fa"
 
+// Styled Component
+import styled from "styled-components"
+
 function App() {
   return (
     <>
         <h1> <FaHome /> Techazon</h1>
+        <Button>Click Me</Button>
     </>
   );
 }
+
+const Button = styled.button`
+  background: var(--primary-color);
+  color: var(--mainWhite);
+  font-size: ${(props) => props.large ? "3rem" : "inherit"};
+`
 
 export default App;
