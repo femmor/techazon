ea02d7bd8977023e8452a3ba35e43ef89258c603
Emmanuel Egomson <femmor2u@gmail.com>
2020-06-17 16:46:25 -0500

    implemented react-icons

--- src/App.js
+++ src/App.js
@@ -2,10 +2,13 @@ import React from 'react';
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css"
 
+// FontAwesome
+import { FaHome } from "react-icons/fa"
+
 function App() {
   return (
     <>
-        <h1>Techazon</h1>
+        <h1> <FaHome /> Techazon</h1>
     </>
   );
 }
