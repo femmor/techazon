c7e78b8ce8868b39e77ac4d685d9c9fac789a226
Emmanuel Egomson <femmor2u@gmail.com>
2020-06-18 08:08:09 -0500

    completed the sidecart open and close functionality

--- src/components/SideCart.jsx
+++ src/components/SideCart.jsx
@@ -1,11 +1,37 @@
 import React from 'react';
+import { ProductConsumer } from "../context"
+import styled from "styled-components"
 
 const SideCart = () => {
     return (
-        <div>
-            <div>SideCart</div>
-        </div>
+        <ProductConsumer>
+            {value => {
+                const { cartOpen, closeCart, cart, cartItems } = value
+                return (
+                    <CartWrapper show={cartOpen} onClick={closeCart}>
+                        <p>Cart items: {cartItems}</p>
+                    </CartWrapper>
+                )
+            }}
+        </ProductConsumer>
     );
 }
+
+const CartWrapper = styled.nav`
+    position: fixed;
+    top: 60px;
+    right: 0;
+    width: 100%;
+    height: 100%;
+    background: var(--mainWhite);
+    z-index: 1;
+    border-left: 2px solid var(--primary-color);
+    transition: var(--mainTransition);
+    transform: ${props => (props.show ? "translateX(0)" : "translateX(100%)")};
+
+    @media (min-width:576px) {
+        width: 20rem;
+    }
+`
 
 export default SideCart;
--- src/context/context.js
+++ src/context/context.js
@@ -11,7 +11,8 @@ class ProductProvider extends Component {
         sidebarOpen: false,
         cartOpen: false,
         cartItems: 10,
-        links: linkData
+        links: linkData,
+        cart: []
     }
 
     // handleSidebar
