639d0b73ddcf3ed8ee4f5335ea313c8e2501d883
Emmanuel Egomson <femmor2u@gmail.com>
2020-06-18 15:39:43 -0500

    created the default error page

--- src/pages/DefaultPage.jsx
+++ src/pages/DefaultPage.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import Hero from "../components/Hero"
+import defaultBcg from "../images/mainBcg.jpeg"
+import { Link } from "react-router-dom"
 
 const DefaultPage = () => {
     return (
         <>
-           <h1>Default page</h1> 
+          <Hero img={defaultBcg} title="404 page not found!" description="we can't seem to find the page you are looking for" max>
+            <Link className="main-link" to="/" style={{
+                marginTop: "2rem"
+            }}>Back to Homepage</Link>
+          </Hero>
         </>
     );
 }
