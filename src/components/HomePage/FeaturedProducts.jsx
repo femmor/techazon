f1b81d3c3abd979382ecc4acf3b11bbb2aa55a8c
Emmanuel Egomson <femmor2u@gmail.com>
2020-06-19 17:47:38 -0500

    created the featured product section

--- src/components/HomePage/FeaturedProducts.jsx
+++ src/components/HomePage/FeaturedProducts.jsx
@@ -1,10 +1,29 @@
 import React from 'react';
+import {Link} from "react-router-dom"
+import Title from "../Title"
+import { ProductConsumer } from "../../context"
+
+// Components
+import Product from "../Product"
 
 const FeaturedProducts = () => {
     return (
-        <div>
-            Hello from FeaturedProducts
-        </div>
+        <section className="py-5">
+            <div className="container">
+                <Title title="featured products" center/>
+                {/* Product */}
+                <div className="row">
+                    <ProductConsumer>
+                        {value => {
+                            const { featuredProducts } = value
+                            return featuredProducts.map(product => (
+                                <Product key={product.id} product={product}/>
+                            ))
+                        }}
+                    </ProductConsumer>
+                </div>
+            </div>
+        </section>
     );
 }
 
