4b6505cb54a20de3d1093ddb7c04cdcac73dba9c
Emmanuel Egomson <femmor2u@gmail.com>
2020-07-01 20:38:49 -0500

    added the cartItem in cartList functionality

--- src/components/CartPage/CartItem.jsx
+++ src/components/CartPage/CartItem.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const CartItem = () => {
     return (
         <div>
-            CartItems
+            working!
         </div>
     );
 }
--- src/components/CartPage/CartList.jsx
+++ src/components/CartPage/CartList.jsx
@@ -1,11 +1,46 @@
 import React from 'react';
+import { ProductConsumer } from "../../context"
+import { Link } from "react-router-dom"
 import CartItem from "./CartItem"
 
 const CartList = () => {
     return (
         <div>
-            CartList
-            <CartItem/>
+            <div className="container-fluid">
+                <div className="row">
+                    <div className="col">
+                        <ProductConsumer>
+                            { value => {
+                                const { cart, increment, decrement, removeItem } = value
+                                if (cart.length < 1) {
+                                    return (
+                                        <>
+                                            <h5 className="text-title text-center my-4">
+                                                your cart is currently empty.
+                                            </h5>
+                                            <div className="text-center">
+                                                <Link to="/products" className="main-link">start shopping</Link>
+                                            </div>
+                                        </>
+                                    )
+                                } 
+                                return (
+                                    <>
+                                        {
+                                            cart.map(item => {
+                                                return (
+                                                    <CartItem key={item.id} cartItem={item} increment={increment} decrement={decrement} removeItem={removeItem}></CartItem>
+                                                )
+                                            })
+                                        }
+                                    </>
+                                )                          
+                               
+                            }}
+                        </ProductConsumer>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
